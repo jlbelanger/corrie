8ecc0f7692347b1e3a8b5a1b61915db80ca5d107
Jenny Belanger <jbelangr@gmail.com>
2022-06-07 14:21:26 -0400

    Update person observer

--- app/Observers/PersonObserver.php
+++ app/Observers/PersonObserver.php
@@ -10,20 +10,40 @@ class PersonObserver
 	 * @param  Person $person
 	 * @return void
 	 */
+	public function updating(Person $person)
+	{
+		if (!$person->isDirty('slug')) {
+			return;
+		}
+
+		// Rename files to match new slug.
+		if ($person->filename) {
+			$newFilename = $person->uploadedFilename('filename', $person->filename);
+			$oldFilename = preg_replace('/\/' . $person->slug . '\./', '/' . $person->getOriginal('slug') . '.', $newFilename);
+			$oldPath = public_path($oldFilename);
+			$newPath = public_path($newFilename);
+			if (file_exists($oldPath) && !file_exists($newPath)) {
+				rename($oldPath, $newPath);
+				$person->filename = $newFilename;
+			}
+		}
+	}
+
+	/**
+	 * @param  Person $person
+	 * @return void
+	 */
 	public function updated(Person $person)
 	{
-		if (!$person->isDirty('filename')) {
-			return;
-		}
-
-		$filename = $person->getOriginal('filename');
-		if (!$filename) {
-			return;
-		}
-
-		$path = public_path($filename);
-		if (file_exists($path)) {
-			unlink($path);
+		// When uploading or removing file, delete the old file.
+		if ($person->isDirty('filename')) {
+			$filename = $person->getOriginal('filename');
+			if ($filename) {
+				$path = public_path($filename);
+				if (file_exists($path)) {
+					unlink($path);
+				}
+			}
 		}
 	}
 
@@ -33,13 +53,17 @@ class PersonObserver
 	 */
 	public function deleted(Person $person)
 	{
-		if (!$person->filename) {
-			return;
+		// Delete associated files.
+		if ($person->filename) {
+			$path = public_path($person->filename);
+			if (file_exists($path)) {
+				unlink($path);
+			}
 		}
 
-		$path = public_path($person->filename);
-		if (file_exists($path)) {
-			unlink($path);
-		}
+		// Rename slug to allow new rows to be created with same slug.
+		$person->slug = 'deleted-' . strtotime('now') . '-' . $person->slug;
+		$person->filename = null;
+		$person->save();
 	}
 }
