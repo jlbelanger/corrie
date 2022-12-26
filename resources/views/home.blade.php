67fee6efea9d6666f87d6bf0a6c0177df6c5c74f
Jenny Belanger <jbelangr@gmail.com>
2022-12-26 10:48:42 -0500

    Update dependencies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
 		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="/favicon.png">
 		<link rel="icon" href="/favicon.ico">
-		<link rel="stylesheet" href="/assets/css/style.min.css?20221015">
+		<link rel="stylesheet" href="/assets/css/style.min.css?20221224">
 		<link rel="manifest" href="/manifest.json">
 	</head>
 	<body>
@@ -75,6 +75,6 @@
 		window.selectedP1 = {!! json_encode($p1) !!};
 		window.selectedP2 = {!! json_encode($p2) !!};
 		</script>
-		<script src="{{ url('/assets/js/functions.min.js?20221015') }}"></script>
+		<script src="{{ url('/assets/js/functions.min.js?20221224') }}"></script>
 	</body>
 </html>
